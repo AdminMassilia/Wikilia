7e6ab08447c9f6af846fa342ec99922cafa0df86
Idriss <idrissdiallo95@gmail.com>
2025-06-28 21:13:25 +0000

    fix production

--- wiki/client/components/common/nav-header.vue
+++ wiki/client/components/common/nav-header.vue
@@ -368,7 +368,7 @@ export default {
     },
     loginRedirectUrl() {
       return (
-        process.env.NUXT_ENV_LOGIN_REDIRECT_URL ||
+        process.env.LOGIN_REDIRECT_URL ||
         "http://localhost:3001/login/ce8967c2-5f64-4259-87d9-85ba9ebcfdd2"
       );
     }
